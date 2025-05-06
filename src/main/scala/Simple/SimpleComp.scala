e39afdeb7a6a573f35782ea88f0e14d607250e8f
arch_ap_007 <rockcong@gmail.com>
2025-05-06 14:03:27 +0800

    add one simple case about basic components

---
+++ src/main/scala/Simple/SimpleComp.scala
@@ -0,0 +1,76 @@
+package Simple
+
+import chisel3._
+// _root_ disambiguates from package chisel3.util.circt if user imports chisel3.util._
+import _root_.circt.stage.ChiselStage
+
+class CompA extends Module {
+  val io = IO(new Bundle {
+    val a = Input(UInt(8.W))
+    val b = Input(UInt(8.W))
+    val x = Output(UInt(8.W))
+    val y = Output(UInt(8.W))
+  })
+  val a1=io.a
+  val b1=io.b
+  io.x :=  a1
+  io.y :=  b1
+}
+class CompB extends Module {
+  val io = IO(new Bundle {
+    val in1 = Input(UInt(8.W))
+    val in2 = Input(UInt(8.W))
+    val out = Output(UInt(8.W))
+  })
+
+  io.out := io.in1+ io.in2
+
+  // function of B
+}
+
+
+class CompC extends Module {
+  val io = IO(new Bundle {
+    val inA = Input(UInt(8.W))
+    val inB = Input(UInt(8.W))
+    val inC = Input(UInt(8.W))
+    val outX = Output(UInt(8.W))
+    val outY = Output(UInt(8.W))
+  })
+
+  // create components A and B
+  val compA = Module(new CompA())
+  val compB = Module(new CompB())
+
+  // connect A
+  compA.io.a := io.inA
+  compA.io.b := io.inB
+  io.outX := compA.io.x
+
+  // connect B
+  compB.io.in1 := compA.io.y
+  compB.io.in2 := io.inC
+  io.outY := compB.io.out
+}
+
+
+class CompD extends Module {
+  val io = IO(new Bundle {
+    val in = Input(UInt(8.W))
+    val out = Output(UInt(8.W))
+  })
+  // function of D
+}
+
+
+
+object SimpleCompApp extends App {
+
+  ChiselStage.emitSystemVerilogFile(
+    new CompC,
+    firtoolOpts = Array(
+      "-o", "generated/SimpleComp.v",
+      "-disable-all-randomization",
+      "-strip-debug-info")
+  )
+}
